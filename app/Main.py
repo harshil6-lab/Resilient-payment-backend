9941678a382516cbded31dbb8a6c1a60bed0aff4
harshil6-lab <harshilkalsariya6@gmail.com>
2026-01-18 18:07:01 +0530

    feat : Initialize FastAPI server with health endpoint

---
+++ app/Main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+app = FastAPI(title = "RESILIENT-PAYMENT-BACKEND") #understand this line -> # This line initializes a FastAPI application instance with the title "RESILIENT-PAYMENT-BACKEND".
+
+@app.get("/health")
+def health_check():
+    return {"status" : "Good"}
+
+#Understaning : what is endpoint ? -> An endpoint in web development refers to a specific URL or URI (Uniform Resource Identifier) where a web service or API can be accessed by clients. It represents a specific function or resource that the server exposes to handle requests from clients. Each endpoint corresponds to a particular operation, such as retrieving data, submitting data, updating information, or deleting resources. Endpoints are typically defined by the combination of the HTTP method (GET, POST, PUT, DELETE, etc.) and the URL path.
