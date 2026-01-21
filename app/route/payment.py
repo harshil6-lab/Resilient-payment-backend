4a6986acbdb9582c8eeb0c8cd273c5fcc7cef247
harshil6-lab <harshilkalsariya6@gmail.com>
2026-01-22 00:00:10 +0530

    feat : Fixing logical issue inside payment

--- app/route/payment.py
+++ app/route/payment.py
@@ -14,7 +14,7 @@ def intitiate_payment(request: PaymentRequest):
     logging.info("Payment intiation request received for user_id : %s | Amount : %s",request.user_id,request.amount)
 
     if request.amount <= 0:
-        logging.error("Invalid Payment Amount : %s from user_id : %s",request.amount,request.user_id)
+        logging.error("Invalid Payment Amount is : %s from user_id : %s",request.amount,request.user_id)
         raise HTTPException(status_code=400, detail="Invalid payment amount, Amount must be greater than zero.")
     
 
